28e0ddbb3c8f06bae86fe48f1dbab7680c288b5b
Kavy Jain <kavyjain15@gmail.com>
2026-07-12 18:58:11 +0530

    Time: 48 ms (68.45%), Space: 54.6 MB (15.67%) - LeetHub

---
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+
+    return async function(...args) {
+
+        return Promise.race([
+
+            fn(...args),
+
+            new Promise((_, reject)=>
+                setTimeout(()=>reject("Time Limit Exceeded"), t)
+            )
+
+        ]);
+
+    }
+
+};
