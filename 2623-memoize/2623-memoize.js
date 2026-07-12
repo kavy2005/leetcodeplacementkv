7dec4244f254fe41a6cc453c808d26590f69cc47
Kavy Jain <kavyjain15@gmail.com>
2026-07-12 18:58:29 +0530

    Time: 266 ms (46.91%), Space: 94 MB (82.01%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+
+    let cache = {};
+
+    return function(...args){
+
+        let key = JSON.stringify(args);
+
+        if(key in cache)
+            return cache[key];
+
+        cache[key] = fn(...args);
+
+        return cache[key];
+
+    }
+
+}
