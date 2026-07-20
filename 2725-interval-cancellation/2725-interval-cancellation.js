e7aa10f04bffcc7801fbd97d03fae8c5c84f412c
Kavy Jain <kavyjain15@gmail.com>
2026-07-20 11:02:53 +0530

    Time: 64 ms (22.48%), Space: 52.8 MB (97.59%) - LeetHub

---
+++ 2725-interval-cancellation/2725-interval-cancellation.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn,args,t) {
+
+    fn(...args);
+
+    const id=setInterval(()=>{
+
+        fn(...args);
+
+    },t);
+
+    return function(){
+
+        clearInterval(id);
+
+    }
+
+};
