aa0c922662acc7478bf128c8397fe465267f9734
Kavy Jain <kavyjain15@gmail.com>
2026-07-21 13:34:45 +0530

    Time: 2 ms (96.25%), Space: 48.6 MB (51.52%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,17 +1,10 @@
 class Solution {
     public int[] countBits(int n) {
-        int arr[]=new int[n+1];
-        for(int i=0;i<=n;i++){
-            int bits=0;
-            int mask=1;
-            for(int j=0;j<32;j++){
-                if((i&mask)!=0)
-                    bits++;
-                mask<<=1;    
-            }
-            arr[i]=bits;
-        }
-        return arr;
-
+        int []bits=new int[n+1];
+        bits[0]=0;
+        for(int i=1;i<=n;i++)
+            bits[i]=bits[i >> 1] + (i & 1);
+        return bits;    
     }
+    
 }
