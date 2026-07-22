fbba6a8fac6ddbfa656e11bd03dbbe39943fd8f8
Kavy Jain <kavyjain15@gmail.com>
2026-07-22 22:04:55 +0530

    Time: 43 ms (66.14%), Space: 55.7 MB (8.8%) - LeetHub

---
+++ 2695-array-wrapper/2695-array-wrapper.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ */
+var ArrayWrapper=function(nums){
+
+    this.nums=nums;
+
+};
+
+ArrayWrapper.prototype.valueOf=function(){
+
+    return this.nums.reduce((a,b)=>a+b,0);
+
+};
+
+ArrayWrapper.prototype.toString=function(){
+
+    return "["+this.nums.join(",")+"]";
+
+};
