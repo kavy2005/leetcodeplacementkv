99eff4ec206d657f0f2a9a33100cc31e300ef8df
Kavy Jain <kavyjain15@gmail.com>
2026-09-15 01:06:22 +0530

    Time: 0 ms (100%), Space: 42.6 MB (89.22%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -21,21 +21,16 @@ class Solution {
 
     private int robRange(int[] nums, int start, int end) {
 
-        int prev2 = 0;
-        int prev1 = 0;
-
-        for (int i = start; i <= end; i++) {
-
-            int take = nums[i] + prev2;
-
-            int notTake = prev1;
-
-            int curr = Math.max(take, notTake);
-
-            prev2 = prev1;
-            prev1 = curr;
+        int []dp=new int [nums.length];
+        dp[start]=nums[start];
+        if(start+1<=end){
+            dp[start+1]=Math.max(nums[start],nums[start+1]);
         }
-
-        return prev1;
+        for(int i=start+2;i<=end;i++){
+            int take=nums[i]+dp[i-2];
+            int nottake=dp[i-1];
+            dp[i]=Math.max(take,nottake);
+        }
+        return dp[end];
     }
 }
